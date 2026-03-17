11bbed7e8d7290524c6efc43dd8f577744ca5ed0
abov <khachatryanabov@gmil.com>
2026-03-17 14:41:27 +0400

    Add PagingConstants

---
+++ app/src/main/java/com/abov/moviehub/data/util/PagingConstants.kt
@@ -0,0 +1,4 @@
+package com.abov.moviehub.data.util
+
+const val DEFAULT_PAGE_SIZE = 20
+
--- app/src/main/java/com/abov/moviehub/domain/usecase/GetPagedMoviesUseCase.kt
+++ app/src/main/java/com/abov/moviehub/domain/usecase/GetPagedMoviesUseCase.kt
@@ -2,6 +2,7 @@ package com.abov.moviehub.domain.usecase
 
 import androidx.lifecycle.LiveData
 import androidx.paging.PagingData
+import com.abov.moviehub.data.util.DEFAULT_PAGE_SIZE
 import com.abov.moviehub.domain.model.Movie
 import com.abov.moviehub.domain.repository.MovieRepository
 import javax.inject.Inject
@@ -9,6 +10,6 @@ import javax.inject.Inject
 class GetPagedMoviesUseCase @Inject constructor(
     private val repository: MovieRepository
 ) {
-    operator fun invoke(pageSize: Int = 20): LiveData<PagingData<Movie>> =
+    operator fun invoke(pageSize: Int = DEFAULT_PAGE_SIZE): LiveData<PagingData<Movie>> =
         repository.getMoviesPaged(pageSize)
 }
